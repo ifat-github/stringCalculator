592542df4e1d83e0249aa42d01cd43c3d2736173
Idan Melamed <idanmel@gmail.com>
2021-06-14 15:21:29 +0300

    handle new line delimiter

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+import re
+
 
 def add(numbers):
     result = 0
+    delimiter = '[,\n]'
+    m = re.search(r'\/\/(.)\n(.*)$', numbers, re.IGNORECASE | re.MULTILINE)
+    if m:
+        delimiter = m.group(1)
+        numbers = m.group(2)
+
     if numbers:
-        result = sum([int(x) for x in numbers.split(',')])
+        result = sum([int(x) for x in re.split(delimiter, numbers)])
     return result
 
 
--- tests/test_stringcalculator.py
+++ tests/test_stringcalculator.py
@@ -12,3 +12,14 @@ def test_single_number_should_return_itself():
 def test_multiple_numbers_should_return_sum():
     assert add('1,2') == 3
     assert add('152,26') == 178
+    assert add('1,2,3') == 6
+
+
+def test_newline_delimiters():
+    assert add('1\n2,3') == 6
+
+
+# def test_different_delimiters():
+#     assert add('//;\n1;2') == 3
+#     assert add('//a\n1a2') == 3
+#     assert add('//?\n1?2') == 3
