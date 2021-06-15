e50f45b4e4cc79cfa621ac11eb74c31e2a1242cb
ifat-webhose <ifat@webhose.io>
2021-06-15 12:03:45 +0300

    code refactoring

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 import re
 
 
-def add(numbers):
-    result = 0
+def get_delimiter(str):
     delimiter = '[,\n]'
-    m = re.search(r'\/\/(.)\n(.*)$', numbers, re.IGNORECASE | re.MULTILINE)
+    special_chars = '.\+*?[^]$(){}=!|:-'
+    m = re.search(r'\/\/(.)', str, re.IGNORECASE | re.MULTILINE)
     if m:
         delimiter = m.group(1)
-        numbers = m.group(2)
+        if delimiter in special_chars:
+            delimiter = '\\' + delimiter
+    return delimiter
+
+
+def parse_numbers(input):
+    numbers = input
+    m = re.search(r'\/\/.\n(.*)$', input, re.IGNORECASE | re.MULTILINE)
+    if m:
+        numbers = m.group(1)
+    return numbers
+
+
+def add(input):
+    result = 0
+    numbers = parse_numbers(input)
+    delimiter = get_delimiter(input)
 
     if numbers:
         result = sum([int(x) for x in re.split(delimiter, numbers)])
@@ -16,4 +32,4 @@ def add(numbers):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    add('')
+    add('1,2')
--- tests/test_stringcalculator.py
+++ tests/test_stringcalculator.py
@@ -19,7 +19,8 @@ def test_newline_delimiters():
     assert add('1\n2,3') == 6
 
 
-# def test_different_delimiters():
-#     assert add('//;\n1;2') == 3
-#     assert add('//a\n1a2') == 3
-#     assert add('//?\n1?2') == 3
+def test_different_delimiters():
+    assert add('//;\n1;2') == 3
+    assert add('//a\n1a2') == 3
+    assert add('//?\n1?2') == 3
+    assert add('//$\n1$2') == 3
